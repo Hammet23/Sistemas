f65329041cdc00ab32da24919267efad9bb70a73
Hammet23 <33499573+Hammet23@users.noreply.github.com>
2018-07-12 16:47:39 +0200

    Add files via upload

---
+++ Intro_SO/Proyecto_2/ccopiar.c
@@ -0,0 +1,10 @@
+// Similar a cat. Version con funciones de biblioteca
+
+#include <stdio.h>
+
+int main(int argc, const char **argv)
+{
+	int c;
+	while ((c=getchar()) != EOF) putchar(c);
+	return 0;
+}
